9d3a4f2b7023aaf8bf250816dc1acb3908146a56
manegit <m1n@gmx.net>
2023-06-14 17:26:32 +0000

    add

--- save.sh
+++ save.sh
@@ -1,3 +1,3 @@
-docker image save -o /tmp/docker-debian-custom.tar ghcr.io/manegit/docker-debian-custom:main
+docker image save -o /tmp/docker-debian-custom.tar ghcr.io/manegit/docker-debian-custom:latest
 gzip /tmp/docker-debian-custom.tar
 mv -f /tmp/docker-debian-custom.tar.gz ~/
